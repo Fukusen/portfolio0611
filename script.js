eca3db899332c6cbc0f971a3739c0bb34992498e
fukusen <academicforone01@gmail.com>
2022-06-24 12:10:42 +0900

    リポジトリの新規作成

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('#home').click(function() {
+        $('html, body').scrollTop(0);
+    });
+    $('#profile').click(function() {
+        $('html, body').scrollTop(1350);
+    });
+    $('#benefit').click(function() {
+        $('html, body').scrollTop(2100);
+    });
+    $('#question').click(function() {
+        $('html, body').scrollTop(4500);
+    })
+    $('#contact').click(function() {
+        $('html, body').scrollTop(3500);
+    });
+
+});
